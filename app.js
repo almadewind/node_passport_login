ee9ee8366fcc5b872a754627dd145665e69223b7
Mario Jerez <almadewind@gmail.com>
2020-06-06 15:13:15 +0200

    Register showing errors filter

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// DB config
+const db = require('./config/keys').MongoURI;
+
+// Connect to Mongo Atlas
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Body Parser
+app.use(express.urlencoded({ extended: false }));
+
 // Routes
 app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 const PORT = process.env.PORT || 5000;
 
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = { 
+    MongoURI:'mongodb+srv://mario1234:mario1234@devcamper-056mm.mongodb.net/mongo-passport-login?retryWrites=true&w=majority'
+}
